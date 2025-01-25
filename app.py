87aee8cd17a820db806b8a95e934fa3a7143c00f
rushitchandani <evital.rushitc@gmail.com>
2025-01-25 17:37:22 +0530

    final build

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ try: # >= win 8.1
 except: # win 8.0 or less
     ctypes.windll.user32.SetProcessDPIAware()
 ## spalsh comment
-# import pyi_splash
-# pyi_splash.update_text('UI Loaded ...')
-# pyi_splash.close()
+import pyi_splash
+pyi_splash.update_text('UI Loaded ...')
+pyi_splash.close()
 
 from datetime import datetime
 log_filename = f"./lib/app_logs.txt"
--- lib/constants.py
+++ lib/constants.py
@@ -10,18 +10,22 @@ HOST = "localhost"
 TALLY_URL = F"http://{HOST}:9000"
 COMPANY_NAME = "abc"    
 
-EVITAL_RX_HOST = "192.168.29.107"
+# EVITAL_RX_HOST = "192.168.29.107"
 # EVITAL_RX_HOST = "192.168.1.46"
-EVITAL_RX_HOST = "localhost"
+# EVITAL_RX_HOST = "localhost"
 # EVITAL_RX_HOST = "192.168.1.29"
 # EVITAL_RX_HOST = "0f9d-2405-201-2000-a0d0-b81f-b3cc-cee8-c796.ngrok-free.app"
-EVITAL_RX_HOST = "06d9-2405-201-2000-a0d0-91e4-84d9-e654-2060.ngrok-free.app"
-# EVITAL_RX_HOST = "api.evitalrx.in"
-# EVITAL_RX_URL = "https://dev-api.evitalrx.in:3050/"
-# EVITAL_RX_URL = f"https://{EVITAL_RX_HOST}:4050/"
-EVITAL_RX_URL = f"http://{EVITAL_RX_HOST}:4000/"
-EVITAL_RX_URL = f"http://{EVITAL_RX_HOST}/"
+# EVITAL_RX_HOST = "06d9-2405-201-2000-a0d0-91e4-84d9-e654-2060.ngrok-free.app"
+# EVITAL_RX_URL = f"http://{EVITAL_RX_HOST}:4000/"
+# EVITAL_RX_URL = f"http://{EVITAL_RX_HOST}/"
 # EVITAL_RX_API_KEY = "masteraYQ8DVYzLP559sGJeWty4wXHaw"
+
+# EVITAL_RX_URL = "https://dev-api.evitalrx.in:3050/"
+
+# live
+EVITAL_RX_HOST = "api.evitalrx.in"
+EVITAL_RX_URL = f"https://{EVITAL_RX_HOST}:4050/"
+
 EVITAL_RX_API_KEY = ""
 
 LOGIN_RESPONSE = {}
