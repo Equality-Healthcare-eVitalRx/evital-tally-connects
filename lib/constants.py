c5af0f2954c57be985d43bfb58b6791be599402f
Smit Chauhan <evital.smitc@gmail.com>
2026-08-31 15:13:07 +0530

    made small changes

--- lib/constants.py
+++ lib/constants.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import sys
 
+# variables
 HOST = "localhost"
 TALLY_URL = f"http://{HOST}:"
 TALLY_PORT = 9000
 
 COMPANY_NAME = "abc"
-    
 envtype = "local"
 
 def _read_version():
